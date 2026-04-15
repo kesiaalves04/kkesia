5beb1d36eb7bacd5b08f2b875ce64f8cadecb1a4
kesia alves <kesia.alves@academico.ifpb.edu.br>
2026-04-15 11:45:55 -0300

    Add files via upload

---
+++ teste1.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.image("foto.kesia")
+st.write("Site Kesia Alves")
+st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/trabalhos-de-poo/in%C3%ADcio")
