4d956b2ec37ec1ce9aaa6dcc44e4b2161c4d5ee9
kesia alves <kesia.alves@academico.ifpb.edu.br>
2026-04-27 11:47:39 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.image("foto.kesia.jpeg")
 st.write("Site Kesia Alves")
 st.link_button("Acessar", "https://sites.google.com/academico.ifpb.edu.br/trabalhos-de-poo/in%C3%ADcio")
+st.link_button("Acessar","https://web.whatsapp.com/")
